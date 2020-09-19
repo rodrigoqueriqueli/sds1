ed47b0ccf1fca012ed7f796ece93f01178e6238b
Rodrigo Queriqueli <rdqueriqueli@gmail.com>
2020-09-19 09:10:25 -0300

    Creating home page

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -6,8 +6,8 @@ const Header = ()  => (
     <header className="main-header">
         <Logo />
         <div className="logo-text"> 
-        <span className="logo-text-1">Big Game</span>   
-        <span className="logo-text-2"> Survey </span>   
+            <span className="logo-text-1">Big Game</span>   
+            <span className="logo-text-2"> Survey</span>   
         </div>
     </header>
 );
