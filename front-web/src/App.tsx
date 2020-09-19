94d80a8e9a0436900088d895c0b45c079d37295e
Rodrigo Queriqueli <rdqueriqueli@gmail.com>
2020-09-19 07:38:25 -0300

    Adding header component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1> Hello Semana Dev Superior</h1>
+    //react só consegue exportar um elemento por componente
+    <div>
+      <Header   />
+    </div>
   );
 }
 
